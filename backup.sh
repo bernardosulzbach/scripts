7d4c457cc273b92319cf49b31e634119f048a58a
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-12-18 13:13:11 -0200

    Removed directory with whitespace from backup.sh

--- backup.sh
+++ backup.sh
@@ -6,8 +6,7 @@ BACKUP_FULL_PATH=$BACKUP_PATH$BACKUP_FILE_NAME
 
 DOCUMENTS="/home/mafagafogigante/Dropbox/Documents/"
 SPREADSHEETS="/home/mafagafogigante/Dropbox/Spreadsheets/"
-SAVES="/home/mafagafogigante/The Escapists/"
 
 mkdir -p $BACKUP_PATH
 cd $BACKUP_PATH
-tar --absolute-names -zcf $BACKUP_FILE_NAME $DOCUMENTS $SPREADSHEETS $SAVES
+tar --absolute-names -zcf $BACKUP_FILE_NAME $DOCUMENTS $SPREADSHEETS
